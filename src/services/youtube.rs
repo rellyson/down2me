627933d7387505c708f29da24ccdc977c8178e83
Rellyson Silva <rellysonsilva@gmail.com>
2021-02-15 14:55:48 -0300

    ytb service struct and serialization added

--- src/services/youtube.rs
+++ src/services/youtube.rs
@@ -1,17 +1,38 @@
-use std::io;
+use serde::{Deserialize, Serialize};
 use serde_json::json;
+use std::io;
+
 use youtube_dl::YoutubeDl;
 
-pub type YoutubeDlResult = Result<serde_json::Value, io::Error>;
-
-#[tokio::main]
-pub async fn download_video(video_url: String) -> YoutubeDlResult {
-    let output = json!(YoutubeDl::new(video_url)
-    .socket_timeout("15")
-    .run()
-    .unwrap());
-        
-    return Ok(output);
+#[derive(Serialize, Deserialize, Debug)]
+struct YoutubeDlData {
+    SingleVideo: VideoInfo,
 }
 
-// let request_url = format!("https://www.youtube.com/get_video_info?video_id={}", video_url);
+#[derive(Serialize, Deserialize, Debug)]
+struct VideoInfo {
+    thumbnail: String,
+    title: String,
+    formats: Vec<Format>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Format {
+    url: String,
+    ext: String,
+    format: String,
+    filesize: Option<i32>,
+    width: Option<i32>,
+    height: Option<i32>,
+}
+
+#[tokio::main]
+pub async fn download_video(video_url: String) -> Result<serde_json::Value, io::Error> {
+    let output = json!(YoutubeDl::new(video_url)
+        .socket_timeout("15")
+        .run()
+        .unwrap());
+
+    let data: YoutubeDlData = serde_json::from_str(&output.to_string())?;
+    return Ok(json!(data.SingleVideo));
+}
